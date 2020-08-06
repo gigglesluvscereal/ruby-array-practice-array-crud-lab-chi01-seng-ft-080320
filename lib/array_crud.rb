8deedd18a2289d9cbfd1f9be3a2dacbcddbccb50
David Gonzales <dsg2386@gmail.com>
2020-08-06 01:16:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  four_elements.push("lighting")
+  array << "lighting"
 end
 
 def add_element_to_start_of_array(array, element)
